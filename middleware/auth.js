76145d4d45b6f467b23c186d5d0b5b098d42c783
SiqiWang666 <siqi_astrid@gwu.edu>
2019-08-07 11:42:56 -0400

    Add json web token authentication middleware.

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+/* This middleware varify the json web token. 
+*/
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+    //Get token.
+    const token = req.header('x-auth-token');
+
+    //Check if not token
+    if(!token) {
+        return res.status(401).json({
+                msg: 'No token, suthorization denied'
+            })
+    }
+    try {
+        //Attach user to ID.
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded.user;
+        next();
+    } catch(err) {
+        res.status(401).json({
+            msg: 'Taken is not valid'
+        })
+    }
+}
