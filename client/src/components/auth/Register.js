e24ef2ceed452d9f9cf09e32babc6f9db7401604
SiqiWang666 <siqi_astrid@gwu.edu>
2019-08-24 18:05:59 -0400

    Add state of Register component.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -10,29 +11,55 @@ const Register = () => {
 
     const { name, email, password, password2 } = formData;
 
+    //Make a copy of formData and overwrite the item.
+    const onChange = (e) => setFormData({...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if(password !== password2) {
+            console.log('Passwords do not match!')
+        } else {
+            console.log('SUCCESS!');
+        }
+    };
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
             <p className="lead">
-                <i className="fas fa-user"></i> Create Your Account
+                <i className="fas fa-user" />Create Your Account
             </p>
-            <form className="form" action="create-profile.html">
+            <form className="form" onSubmit={e => onSubmit(e)} >
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" required />
+                    <input 
+                        type="text" 
+                        placeholder="Name" 
+                        name="name" 
+                        value={name} 
+                        onChange={e => onChange(e)} 
+                        required 
+                    />
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" />
-                <small className="form-text"
-                    >This site uses Gravatar so if you want a profile image, use a
-                    Gravatar email</small
-                >
+                    <input 
+                        type="email" 
+                        placeholder="Email Address" 
+                        name="email"
+                        value={email} 
+                        onChange={e => onChange(e)}     
+                    />
+                    <small className="form-text">
+                        This site uses Gravatar so if you want a profile image, use a Gravatar email.
+                    </small>
                 </div>
                 <div className="form-group">
                     <input
                         type="password"
                         placeholder="Password"
                         name="password"
-                        minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)} 
+                        minLength="8"
                     />
                 </div>
                 <div className="form-group">
@@ -40,13 +67,15 @@ const Register = () => {
                         type="password"
                         placeholder="Confirm Password"
                         name="password2"
-                        minLength="6"
+                        value={password2} 
+                        onChange={e => onChange(e)} 
+                        minLength="8"
                     />
                 </div>
                 <input type="submit" className="btn btn-primary" value="Register" />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <Link to = '/login'>Sign In</Link>
             </p>
         </Fragment>
     )
