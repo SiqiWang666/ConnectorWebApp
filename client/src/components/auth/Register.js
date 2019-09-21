3adf1d743b5e1b0814bffb350888679e83a23c12
SiqiWang666 <siqi_astrid@gwu.edu>
2019-09-20 23:23:36 -0400

    Integrate register action within register component.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { setAlert }  from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -22,7 +23,7 @@ const Register = ({ setAlert }) => {
         if(password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('SUCCESS!');
+            register({ name, email, password });
         }
     };
 
@@ -85,8 +86,9 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-    setAlert: PropTypes.func.isRequired
+    setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired
 };
 
 //Pass the action you want to connect.
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
