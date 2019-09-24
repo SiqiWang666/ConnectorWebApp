301368ccdc6d047be14b87325b1bf2884af81d9d
SiqiWang666 <siqi_astrid@gwu.edu>
2019-09-23 22:36:33 -0400

    Implement action of loading user.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,25 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL } from '../actions/types';
+import setAuthToken from '../utils/setAuthToken';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from '../actions/types';
+//Load User
+export const loadUser = () => async dispatch => {
+    if(localStorage.token) {
+        setAuthToken(localStorage.token);
+    }
+
+    try{
+        const res = await axios.get('/api/auth');
+        dispatch({
+            type: USER_LOADED,
+            payload: res.data
+        })
+    } catch (err) {
+        dispatch({
+            type: AUTH_ERROR
+        })
+    }
+}
 
 //Register User
 export const register = ({ name, email, password }) => async dispatch => {
