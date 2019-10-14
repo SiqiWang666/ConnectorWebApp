0f380a10b99815e5a2bbb95cd7e98d4db5fb9b91
SiqiWang666 <siqi_astrid@gwu.edu>
2019-10-13 23:14:19 -0400

    Get logged user profile.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { setAlert } from './alert';
 import { GET_PROFILE, PROFILE_ERROR } from './types';
 
-// Get current user's profile.
+// Get current user's profile. Get request to backend.
 export const getCurrentProfile = () => async dispatch => {
     try {
         const res = await axios.get('/api/profile/me');
