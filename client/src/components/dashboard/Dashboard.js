bbf3b9e2269074147450a5a070deb3b192c55cae
SiqiWang666 <siqi_astrid@gwu.edu>
2019-10-16 23:20:56 -0400

    Add welcome of dashboard page.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { getCurrentProfile } from '../../actions/profile'
+import { getCurrentProfile } from '../../actions/profile';
+import Spinner from '../layout/Spinner';
 
-const Dashboard = ({ getCurrentProfile, auth, profile}) => {
+const Dashboard = ({ getCurrentProfile, auth: {user}, profile: { profile, loading }}) => {
     useEffect(() => {
         getCurrentProfile();
     }, []);
-    return (
-        <div>
-            dashboard
-        </div>
-    )
+    return loading && profile === null ? <Spinner /> : 
+    <Fragment>
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+            <i className='fas fa-user'/> Welcome {user && user.name}
+        </p>
+    </Fragment>;
 };
 
 Dashboard.propTypes = {
