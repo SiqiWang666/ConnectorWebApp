9dea02ef645d15f80084dd7f1e59c0873bb7b2b2
SiqiWang666 <siqi_astrid@gwu.edu>
2020-01-05 19:45:57 -0500

    Update post reducer, action & component

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -47,6 +48,7 @@ const App = () => {
               <PrivateRoute exact path = '/edit-profile' component = { EditProfile } />
               <PrivateRoute exact path = '/add-experience' component = { AddExperience } />
               <PrivateRoute exact path = '/add-education' component = { AddEducation} />
+              <PrivateRoute exact path = '/posts' component = { Posts } />
             </Switch>
           </section>
         </Fragment>
