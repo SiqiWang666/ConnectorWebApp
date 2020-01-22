254db9abadab82b5274738ce5544c25fc169ef1d
SiqiWang666 <siqi_astrid@gwu.edu>
2020-01-21 23:50:54 -0500

    Specify folder of static assests for Express.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/dbConnection');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 //Connect database.
@@ -9,11 +10,21 @@ connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+//app.get('/', (req, res) => res.send('API Running'));
 //Define  Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+// Server static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
